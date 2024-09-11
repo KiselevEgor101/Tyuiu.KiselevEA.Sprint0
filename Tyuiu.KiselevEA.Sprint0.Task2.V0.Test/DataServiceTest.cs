c17294a5197acdcc5081d955558e25700f76ac01
Tyuiu.KiselevEA@outlook.com <Tyuiu.KiselevEA@outlook.com>
2024-09-11 15:33:38 +0500

    Task2

---
+++ Tyuiu.KiselevEA.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+using System.Windows.Markup;
+using Tyuiu.KiselevEA.Sprint0.Task2.V0.Lib;
+
+namespace Tyuiu.KiselevEA.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckGetMessageValid()
+        {
+            // Область создания методов тестирования, методов из библиотеки
+            var name = "Егор";
+            var res = DataService.GetMessage(name);
+
+            //Вызываем класс Assert и метод AreEqual
+            Assert.AreEqual("Привет, Егор", res);
+        }
+    }
+}
