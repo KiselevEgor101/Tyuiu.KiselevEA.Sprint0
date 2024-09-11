4c109e2c8226442353d570652b3bf3306a88e4b0
Tyuiu.KiselevEA@outlook.com <Tyuiu.KiselevEA@outlook.com>
2024-09-11 21:43:23 +0500

    Task4

---
+++ Tyuiu.KiselevEA.Sprint0.Task4.V0/Program.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.KiselevEA.Sprint0.Task4.V0.Lib;
+namespace Tyuiu.KiselevEA.Sprint0.Task4.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DataService.Addition(50, 2));
+            Console.WriteLine(DataService.Division(104, 2));
+            Console.WriteLine(DataService.Multiplication(26, 2));
+            Console.WriteLine(DataService.Substraction(54, 2));
+
+            Console.ReadKey();
+        }
+    }
+}
