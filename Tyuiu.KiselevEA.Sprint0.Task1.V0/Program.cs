c4dbbc7683ee46ed6a294c4edbfea2f2f0bebfd3
Tyuiu.KiselevEA@outlook.com <Tyuiu.KiselevEA@outlook.com>
2024-09-11 15:02:41 +0500

    Task1

--- Tyuiu.KiselevEA.Sprint0.Task1.V0/Program.cs
+++ Tyuiu.KiselevEA.Sprint0.Task1.V0/Program.cs
@@ -1,5 +1,11 @@
-﻿Console.WriteLine("Hello, World");
-string name = "Егор";
-int age = 18;
-Console.WriteLine("Здравствуйте меня зовут " + name + " мне " + age + " лет!");
-Console.ReadKey();
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World");
+        string name = "Егор";
+        int age = 18;
+        Console.WriteLine("Здравствуйте меня зовут " + name + " мне " + age + " лет!");
+        Console.ReadKey();
+    }
+}
