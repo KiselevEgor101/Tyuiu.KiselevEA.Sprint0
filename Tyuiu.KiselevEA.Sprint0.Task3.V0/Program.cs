b112eb6a9aab5f37aa73e4e2059b13dec9339d03
Tyuiu.KiselevEA@outlook.com <Tyuiu.KiselevEA@outlook.com>
2024-09-11 15:54:17 +0500

    Task3

---
+++ Tyuiu.KiselevEA.Sprint0.Task3.V0/Program.cs
@@ -0,0 +1,13 @@
+﻿using Tyuiu.KiselevEA.Sprint0.Task3.V0.Lib;
+
+namespace Tyuiu.KiselevEA.Sprint0.Task3.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DataService.Sum(52, 52));
+            Console.ReadKey();
+        }
+    }
+}
