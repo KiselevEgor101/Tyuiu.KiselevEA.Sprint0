6a8773c5902fe60f4bc2e8a06dc1425dfd4d52f0
Tyuiu.KiselevEA@outlook.com <Tyuiu.KiselevEA@outlook.com>
2024-09-12 19:32:50 +0500

    Task5

--- Tyuiu.KiselevEA.Sprint0.Task4.V0/Program.cs
+++ Tyuiu.KiselevEA.Sprint0.Task4.V0/Program.cs
@@ -6,7 +6,7 @@ namespace Tyuiu.KiselevEA.Sprint0.Task4.V0
         static void Main(string[] args)
         {
             Console.WriteLine(DataService.Addition(50, 2));
-            Console.WriteLine(DataService.Division(104, 2));
+            Console.WriteLine(DataService.Division(104, 0));
             Console.WriteLine(DataService.Multiplication(26, 2));
             Console.WriteLine(DataService.Substraction(54, 2));
 
---
+++ Tyuiu.KiselevEA.Sprint0.Task5.V0/Program.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.KiselevEA.Sprint0.Task5.V0.Lib;
+
+namespace Tyuiu.KiselevEA.Sprint0.Task5.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("A + B = " + DataService.Addition(50,2));
+            Console.WriteLine("A + B = " + DataService.Substraction(54, 2));
+            Console.WriteLine("A + B = " + DataService.Multiplication(26, 2));
+            Console.WriteLine("A + B = " + DataService.Division(104, 2));
+            Console.ReadKey();
+        }
+    }
+}
